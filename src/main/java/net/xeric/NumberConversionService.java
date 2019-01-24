853c0291efad98404a12b5ddcc7ee3178b1cb0c4
Mark Shead <mark.shead@xeric.net>
2019-01-24 09:29:09 -0600

    added basic framwork for test exercise

---
+++ src/main/java/net/xeric/NumberConversionService.java
@@ -0,0 +1,7 @@
+package net.xeric;
+
+/**
+ * Created by markshead on 1/24/19.
+ */
+public class NumberConversionService {
+}
---
+++ src/test/java/net/xeric/NumberConversionServiceTest.java
@@ -0,0 +1,65 @@
+package net.xeric;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Created by markshead on 1/24/19.
+ */
+public class NumberConversionServiceTest {
+    NumberConversionService ncs = new NumberConversionService();
+
+    @Test
+    public void testConvert1() {
+        assertThat(true, is(true));
+//        assertThat(ncs.aToR(1), is("I"));
+    }
+
+//    @Test
+//    public void testConvert2() {
+//        assertThat(ncs.aToR(2), is("II"));
+//    }
+//
+//    @Test
+//    public void testConvert3() {
+//        assertThat(ncs.aToR(3), is("III"));
+//    }
+//
+//    @Test
+//    public void testConvert4() {
+//        assertThat(ncs.aToR(4), is("IV"));
+//    }
+//
+//    @Test
+//    public void testConvert5() {
+//        assertThat(ncs.aToR(5), is("V"));
+//    }
+//
+//    @Test
+//    public void testConvert6() {
+//        assertThat(ncs.aToR(6), is("VI"));
+//    }
+//
+//    @Test
+//    public void testConvert7() {
+//        assertThat(ncs.aToR(7), is("VII"));
+//    }
+//
+//    @Test
+//    public void testConvert8() {
+//        assertThat(ncs.aToR(8), is("VIII"));
+//    }
+//
+//    @Test
+//    public void testConvert9() {
+//        assertThat(ncs.aToR(9), is("IX"));
+//    }
+//
+//    @Test
+//    public void testConvert10() {
+//        assertThat(ncs.aToR(10), is("X"));
+//    }
+
+}
